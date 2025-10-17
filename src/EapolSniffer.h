53776cd47f84a9e3d323e9fafd8bcee836240dc5
muylder <65868139+muylder@users.noreply.github.com>
2025-10-17 17:35:53 -0300

    new handshakes

--- src/EapolSniffer.h
+++ src/EapolSniffer.h
@@ -25,7 +25,7 @@ void SnifferEnd();
 const PacketInfo* SnifferGetPacketInfoTable();
 void SnifferDebugMode();
 String getSSIDFromMac(const uint8_t* mac);
-bool isNewHandshake();
+bool isNewHandshake(const uint8_t* currentBSSID);
 bool isEapolFrame(const uint8_t *data, uint16_t len);
 static inline int ieee80211_hdrlen(uint16_t fc);
 uint8_t getEAPOLOrder(uint8_t* buf);
